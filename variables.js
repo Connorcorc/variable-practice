e6d7b76ab33ec5dcff7837c8de5ea7de6a7354ce
Connor Corcoran <connorcorc@gmail.com>
2022-04-13 20:25:14 -0600

    Add annotation

--- variables.js
+++ variables.js
@@ -1,10 +1,12 @@
-var string = "this is a string"
+var cat = "Porcini"
 var otherString = "this is also a string"
 var favoriteAnimal = "cat"
 
-var bigNumber = 100023498879234
+var catAge = 7
 var smallNumber = 1
 var mikeJones = 18003308004
 
-var isTrue = true
+var isFat = true
 var isFalse = false
+
+console.log(cat, catAge, isFat);
