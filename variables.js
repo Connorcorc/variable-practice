bba8ddfb7d4715f126dc9be99bae535b4dcfd1f4
Connor Corcoran <connorcorc@gmail.com>
2022-04-13 20:21:19 -0600

    Add Numbers

--- variables.js
+++ variables.js
@@ -1,3 +1,7 @@
 var string = "this is a string"
 var otherString = "this is also a string"
 var favoriteAnimal = "cat"
+
+var bigNumber = 100023498879234
+var smallNumber = 1
+var mikeJones = 18003308004
